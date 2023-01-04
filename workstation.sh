39b4ac2996f30c25dc77078078107f13c8d4c4bb
Sean Heath <heathsd@pm.me>
2023-01-04 11:32:55 -0500

    added an auto-checker for yay to skip already installed packages

--- workstation.sh
+++ workstation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DODURL="https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/certificates_pkcs7_DoD.zip"
+
 pacmanPackages=(
 	neovim
 	firefox
@@ -15,15 +17,19 @@ pacmanPackages=(
 	unzip
 	wget
 )
+yayPackages=(
+	mullvad-vpn
+	joplin-desktop
+)
 
-# Install packages
+# Install Pacman packages
 sudo pacman -Syyu --noconfirm $pacmanPackages
 
 # Install DoD Certificates
 if not test -d $HOME/.config/DoDCerts; then
 	mkdir -p $HOME/.config/DoDCerts
 	cd $HOME/.config/DoDCerts
-	wget https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/certificates_pkcs7_DoD.zip
+	wget $DODURL
 	unzip certificates_pkcs7_DoD.zip
 	sudo trust anchor $HOME/.config/DoDCerts/Certificates_PKCS7_v5.9_DoD/DoD_PKE_PEM.pem
 fi
@@ -35,15 +41,14 @@ if not test -f /usr/lib/vmware/view/pkcs11/libopenscpkcs11.so; then
 	sudo ln -s /usr/lib64/pkcs11/opensc-pkcs11.so /usr/lib/vmware/view/pkcs11/libopenscpkcs11.so
 fi
 
-# Install mullvad
-if not which mullvad; then
-	yay -S --noconfirm --answerdiff=None mullvad-vpn
-fi
-
-# Install joplin
-if not which joplin-dekstop; then
-	yay -S --noconfirm --answerdiff=None joplin-appimage
-fi
+# Install Yay packages
+for pkg in ${yayPackages[@]}; do
+	if yay -Pc | grep $pkg; then
+		echo "Yay: $pkg already installed"
+		yayPackages=("${yayPackages[@]/$pkg}")
+	fi
+done
+yay -S --noconfirm --answerdiff=None $yayPackages
 
 # Enable services
 sudo systemctl enable --now pcscd
